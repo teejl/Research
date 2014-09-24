2343e596067e7118d3f7d6fc0cce8fe4494ff971
Thomas J Lockwood Jr <tlockwood@math.utexas.edu>
2014-09-24 13:48:28 -0500

    Gathered Data

--- PP/Solutions.py
+++ PP/Solutions.py
@@ -4,9 +4,22 @@ Goal: Find different Polynomial Permutations
 Goddard Research Fall 2014
 '''
 
-def Solutions(a,p):
+def solutions(a,p):
     sol = []
+    powers = []
     for i in range(a):
         c = i**p % a
         sol.append(c)
-    return sol
+        d = set(sol)
+        if len(d) == a:
+            powers.append(p)
+    if len(powers) != 0:
+        print powers, 'for', a
+    return powers
+
+def multiple_solutions(a,p):
+    for i in range(2,p):
+        for k in range(a):
+            solutions(k,i)
+
+multiple_solutions(1000,100)
